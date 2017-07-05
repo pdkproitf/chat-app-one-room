3ca558f438a0597c0b8179635eb6147fa6e860e0
pdkproitf <phucdaica.pro@gmail.com>
2017-07-05 09:10:33 +0700

    turn on actionacble and add messages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'rooms#show'
-
+  mount ActionCable.server => '/cable'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
